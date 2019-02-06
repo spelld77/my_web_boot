407b6cb21cf163f403571ee06514eb753295f1f6
spelld77 <spelld77@gmail.com>
2019-02-06 21:16:20 +0900

    simple todo list

---
+++ 3_javascript/5_todolist.js
@@ -0,0 +1,42 @@
+var todos = ["Buy New Turtle"];
+
+// window.setTimeout(function(){
+
+    do{
+        var input = prompt("What would you like to do?");
+        if(input === "list"){
+            listTodos();
+        } else if(input === "new"){
+            addTodo();
+        } else if(input === "delete"){
+            deleteTodo();
+        }
+    }while(input !== "quit")
+
+
+// }, 500);
+
+function listTodos(){
+    console.log("**********");
+    todos.forEach(function(todo, i){
+        console.log(i + " : " + todo);
+    });
+    console.log("**********");
+}
+
+function addTodo(){
+    //ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //add to todos array
+    todos.push(newTodo);
+    console.log("Added todo");
+}
+
+function deleteTodo(){
+    //ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    //delete that todo
+    //splice()
+    todos.splice(index,1);
+    console.log("Deleted Todo");
+}
